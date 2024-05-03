7d23e82e35a7960361d839b0da3a60db4a6bfe5c
Sagar Kumar <szkumar16@gmail.com>
2024-05-03 13:04:30 -0400

    Updates 05.24

--- src/models/NSI.py
+++ src/models/NSI.py
@@ -3,7 +3,7 @@ import numpy as np
 from tqdm import tqdm
 from tqdm.notebook import tqdm as tqdm_nb
 import random
-from collections import Counter
+from collections import Counter, defaultdict
 
 
 """
@@ -13,6 +13,20 @@ Noisy Susceptible Infected Model as implemented in the paper.
 """
 
 """ Functions """
+
+def count_faster(l):
+    
+    dic = {}
+    
+    for i in l:
+        
+        if i in dic.keys():
+            dic[i] += 1
+            
+        else:
+            dic[i] = 1
+    
+    return [(key, dic[key]) for key in dic.keys()]
 
 
 def di(infected: np.ndarray,
@@ -127,9 +141,10 @@ class NSI:
                                 beta: float,
                                 k: int,
                                 seedI: np.ndarray = None,
-                                density = True,
-                                pbar_on:bool = True,
-                                notebook:bool = False) -> ModelOutput:
+                                density: bool = True,
+                                track_comm: bool = False,
+                                pbar_on: bool = True,
+                                notebook: bool = False) -> ModelOutput:
 
         """
         Runs the homogeneous population simulation of the NSI model as described in the paper.
@@ -140,12 +155,13 @@ class NSI:
         :param k: average degree
         :param seedI: seed distribution (default 1 in message 0).
         :param density: Output infected vectors as fractions of the population
+        :param track_comm: track successful communications
         :param pbar_on: turn on progress bar
         :param notebook: if pbar_on is True, indicates whether to use the notebook implementation of tqdm
         :return: infected vectors at each time step
         """
-
-        # tracks the state/message received by each node
+        
+        # mutable dictionary of the state/message received by each node
         population_dictionary: dict[int, int] = {n: -1 for n in range(self.N)}
 
         if seedI is None:
@@ -156,19 +172,24 @@ class NSI:
             seed: np.ndarray = seedI
 
         inf: list[np.ndarray] = [seed]
+            
+        # intialize list to successful communications
+        comms = {n: [] for n in range(self.N)}
 
         # setting seed(s)
         for d, seed_count in enumerate(seed):
             for _ in range(int(seed_count)):
                 node = random.randint(0, self.N - 1)
                 population_dictionary[node] = d
+                comms[node].append((node,d))
+               
 
         # progress bar
         if pbar_on:
             if notebook:
-                pbar = tqdm_nb(range(self.T))
+                pbar = tqdm_nb(range(1, self.T))
             else:
-                pbar = tqdm(range(self.T))
+                pbar = tqdm(range(1, self.T))
         else:
             pbar = range(self.T)
 
@@ -178,7 +199,11 @@ class NSI:
             it = np.array([0]*self.P.shape[0])
 
             # time step updates
+            
+            # infected nodes
             valid_nodes = [node for node in population_dictionary.keys() if population_dictionary[node] != -1]
+            
+            
 
             while len(valid_nodes) > 0:
 
@@ -186,7 +211,8 @@ class NSI:
                 valid_nodes.remove(n)
 
                 n_state = population_dictionary[n]
-
+                
+                
                 neighbors = np.random.randint(self.N, size=k)
 
                 for ni in neighbors:
@@ -196,28 +222,162 @@ class NSI:
                         ni_state = population_dictionary[ni]
 
                         if ni_state < 0:
-                            population_dictionary[ni] = error_message(n_state, self.P)
+                            new_state = error_message(n_state, self.P)
+                            population_dictionary[ni] = new_state
+
+                            comms[ni] = comms[n].copy()
+                            comms[ni].append((ni, new_state))
 
                         else:
                             pass
 
                     else:
                         pass
+            
+            
+            cts = count_faster(population_dictionary.values())
 
-            cts = Counter(population_dictionary.values())
-
-            for x in cts.keys():
-                if x != -1:
-                    it[x] = cts[x]
+            for x in cts:
+                if x[0] != -1:
+                    it[x[0]] = x[1]
 
             inf.append(it)
+
             
         if density:
             inf = [i/self.N for i in inf]
 
         out = ModelOutput(infected=inf)
+        
+        if track_comm:
+            return out, comms
+        
+        else:
+            return out
+        
+        
+    def homogeneous_simulations_2(self,
+                                      beta: float,
+                                      k: int,
+                                      seedI: np.ndarray = None,
+                                      density: bool = True,
+                                      track_comm: bool = False,
+                                      pbar_on: bool = True,
+                                      notebook: bool = False) -> ModelOutput:
 
-        return out
+        """
+        Runs the homogeneous population simulation of the NSI model as described in the paper.
+
+        Note: -1 is the "susceptible" state, k must be in simulations
+
+        :param beta: transmissibility
+        :param k: average degree
+        :param seedI: seed distribution (default 1 in message 0).
+        :param density: Output infected vectors as fractions of the population
+        :param track_comm: track successful communications
+        :param pbar_on: turn on progress bar
+        :param notebook: if pbar_on is True, indicates whether to use the notebook implementation of tqdm
+        :return: infected vectors at each time step
+        """
+        
+        # mutable dictionary of the state/message received by each node
+        population_dictionary: dict[int, int] = {n: -1 for n in range(self.N)}
+
+        if seedI is None:
+            seed: np.ndarray = np.zeros(self.P.shape[0])
+            seed[0] = 1  # seed one node with message 0
+
+        else:
+            seed: np.ndarray = seedI
+
+        inf: list[np.ndarray] = [seed]
+            
+        # intialize list to successful communications
+        comms = {n: [] for n in range(self.N)}
+
+        # setting seed(s)
+        for d, seed_count in enumerate(seed):
+            for _ in range(int(seed_count)):
+                node = random.randint(0, self.N - 1)
+                population_dictionary[node] = d
+                comms[node].append((node,d))
+               
+
+        # progress bar
+        if pbar_on:
+            if notebook:
+                pbar = tqdm_nb(range(1, self.T))
+            else:
+                pbar = tqdm(range(1, self.T))
+        else:
+            pbar = range(self.T)
+
+        # running simulation
+        for t in pbar:
+
+            it = np.array([0]*self.P.shape[0])
+
+            # time step updates
+            
+            # infected nodes
+            valid_nodes = [node for node in population_dictionary.keys() if population_dictionary[node] != -1]
+            
+            deg_count = {n: c for n in population_dictionary.keys()}
+
+            while len(valid_nodes) > 0:
+
+                n = random.choice(valid_nodes)
+                valid_nodes.remove(n)
+
+                n_state = population_dictionary[n]
+                
+                if (k-deg_count[n]) > 0: 
+                    neighbors = np.random.randint(self.N, size=k-deg_count[n])
+
+                    for ni in neighbors:
+
+                        deg_count[n] += 1
+                        deg_count[ni] += 1
+
+                        if random.random() < beta:
+
+                            ni_state = population_dictionary[ni]
+
+                            if ni_state < 0:
+                                new_state = error_message(n_state, self.P)
+                                population_dictionary[ni] = new_state
+
+                                comms[ni] = comms[n].copy()
+                                comms[ni].append((ni, new_state))
+
+                            else:
+                                pass
+
+                        else:
+                            pass
+                else:
+                    pass
+            
+            
+            cts = count_faster(population_dictionary.values())
+
+            for x in cts:
+                if x[0] != -1:
+                    it[x[0]] = x[1]
+
+            inf.append(it)
+
+            
+        if density:
+            inf = [i/self.N for i in inf]
+
+        out = ModelOutput(infected=inf)
+        
+        if track_comm:
+            return out, comms
+        
+        else:
+            return out
     
 
 
